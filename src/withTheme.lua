8b0e043577adba20c4108410f2560e9a06a64b01
u-train <skassem05@outlook.com>
2022-08-02 16:00:00 -0400

    inital work

--- src/withTheme.lua
+++ src/withTheme.lua
@@ -1,23 +1,75 @@
 local Packages = script.Parent.Parent
 local Roact = require(Packages.Roact)
+local ThemeContext = require(script.Parent.ThemeContext)
 
 local StudioThemeProvider = Roact.Component:extend("StudioThemeProvider")
 local studioSettings = settings().Studio
 
 function StudioThemeProvider:init()
-	self:setState({ theme = studioSettings.Theme })
-	self._changed = studioSettings.ThemeChanged:Connect(function()
-		self:setState({ theme = studioSettings.Theme })
-	end)
+	local theme = self:GetContext(ThemeContext)
+	self:setState({ studioTheme = studioSettings.Theme })
+
+	if theme == nil then
+		self._changed = studioSettings.ThemeChanged:Connect(function()
+			self:setState({ studioTheme = studioSettings.Theme })
+		end)
+	end
 end
 
 function StudioThemeProvider:willUnmount()
-	self._changed:Disconnect()
+	if self._changed then
+		self._changed:Disconnect()
+	end
+end
+
+function StudioThemeProvider:willUpdate()
+	local theme = self:GetContext(ThemeContext)
+	if theme and self._changed then
+		self._changed:Disconnect()
+		self._changed = nil
+	elseif theme == nil then
+		self._changed = studioSettings.ThemeChanged:Connect(function()
+			self:setState({ studioTheme = studioSettings.Theme })
+		end)
+
+		task.defer(self.setState, self, { studioTheme = studioSettings.Theme })
+	end
 end
 
 function StudioThemeProvider:render()
+	local theme = self:GetContext(ThemeContext)
 	local render = Roact.oneChild(self.props[Roact.Children])
-	return render(self.state.theme)
+
+	if theme then
+		return render(theme)
+	else
+		return Roact.createElement(ThemeContext.Provider, {
+			value = self.state.studioTheme,
+		}, {
+			Consumer = Roact.createElement(ThemeContext.Consumer, {
+				render = render,
+			}),
+		})
+	end
+end
+
+-- https://github.com/Kampfkarren/roact-hooks/blob/main/src/createUseContext.lua
+function StudioThemeProvider:GetContext(targetContext)
+	local contextValue = nil
+
+	local fakeConsumer = setmetatable({
+		props = {
+			render = function(value)
+				contextValue = value
+			end,
+		},
+	}, {
+		__index = self,
+	})
+
+	targetContext.Consumer.init(fakeConsumer)
+
+	return contextValue
 end
 
 local function withTheme(render)
