c188d2d5b07dcf0f4cbefb901fe7c73614ec6764
sircfenner <sircfenner@users.noreply.github.com>
2022-09-25 22:36:26 +0100

    background uses hooks

--- src/Background.lua
+++ src/Background.lua
@@ -1,20 +1,22 @@
 local Packages = script.Parent.Parent
+
 local Roact = require(Packages.Roact)
+local Hooks = require(Packages.RoactHooks)
 
-local withTheme = require(script.Parent.withTheme)
+local useTheme = require(script.Parent.useTheme)
 
-local function Background(props)
-	return withTheme(function(theme)
-		return Roact.createElement("Frame", {
-			Size = props.Size or UDim2.fromScale(1, 1),
-			Position = props.Position or UDim2.fromScale(0, 0),
-			AnchorPoint = props.AnchorPoint or Vector2.new(0, 0),
-			LayoutOrder = props.LayoutOrder or 0,
-			ZIndex = props.ZIndex or 1,
-			BorderSizePixel = 0,
-			BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainBackground),
-		}, props[Roact.Children])
-	end)
+local function Background(props, hooks)
+	local theme = useTheme(hooks)
+
+	return Roact.createElement("Frame", {
+		Size = props.Size or UDim2.fromScale(1, 1),
+		Position = props.Position or UDim2.fromScale(0, 0),
+		AnchorPoint = props.AnchorPoint or Vector2.new(0, 0),
+		LayoutOrder = props.LayoutOrder or 0,
+		ZIndex = props.ZIndex or 1,
+		BorderSizePixel = 0,
+		BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainBackground),
+	}, props[Roact.Children])
 end
 
-return Background
+return Hooks.new(Roact)(Background)
