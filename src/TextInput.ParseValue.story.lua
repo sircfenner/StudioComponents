262e100571cd59b1f42c3044c93ef49604800d60
utrain <skassem05@outlook.com>
2022-09-27 12:53:44 -0400

    Make an example usage of the new API A separate story to demostrate the API specifically via numbers. It has a status text that is set on an error occuring.

---
+++ src/TextInput.ParseValue.story.lua
@@ -0,0 +1,64 @@
+local Packages = script.Parent.Parent
+local Roact = require(Packages.Roact)
+local Hooks = require(Packages.RoactHooks)
+
+local TextInput = require(script.Parent.TextInput)
+local Label = require(script.Parent.Label)
+
+local function Helper(props, hooks)
+	local parsingError, setParsingError = hooks.useState()
+	local number, setNumber = hooks.useState(0)
+
+	return Roact.createElement("Frame", {
+		AnchorPoint = Vector2.new(0.5, 0.5),
+		Position = UDim2.fromScale(0.5, 0.5),
+		Size = UDim2.fromScale(0.7, 0.7),
+		BackgroundTransparency = 1,
+	}, {
+		Layout = Roact.createElement("UIListLayout", {
+			Padding = UDim.new(0, 5),
+			SortOrder = Enum.SortOrder.LayoutOrder,
+			FillDirection = Enum.FillDirection.Vertical,
+			HorizontalAlignment = Enum.HorizontalAlignment.Center,
+			VerticalAlignment = Enum.VerticalAlignment.Center,
+		}),
+		StatusText = Roact.createElement(Label, {
+			Size = UDim2.new(1, 0, 0, 21),
+			LayoutOrder = 0,
+			Text = parsingError or "Valid number!",
+			TextColorStyle = if parsingError then Enum.StudioStyleGuideColor.ErrorText else nil,
+		}),
+		Input = Roact.createElement(TextInput, {
+			Value = number,
+			PlaceholderText = "Enabled",
+			LayoutOrder = 1,
+			TryParsing = function(text)
+				local value = tonumber(text)
+
+				if value then
+					return true, value
+				else
+					return false, "Invalid number!"
+				end
+			end,
+			OnChangedInvalid = setParsingError,
+			OnChanged = function(value)
+				if error then
+					setParsingError(Roact.None)
+				end
+
+				setNumber(value)
+			end,
+		}),
+	})
+end
+
+Helper = Hooks.new(Roact)(Helper)
+
+return function(target)
+	local element = Roact.createElement(Helper)
+	local handle = Roact.mount(element, target)
+	return function()
+		Roact.unmount(handle)
+	end
+end
