bb2eab5156ffd403cbeb98ec348abbf555598b95
sircfenner <sircfenner@users.noreply.github.com>
2022-09-27 18:17:28 +0100

    tooltip may go left, up, or both depending on space

--- src/Tooltip.lua
+++ src/Tooltip.lua
@@ -14,6 +14,11 @@ local TEXT_PADDING_SIDES = 3
 local TEXT_PADDING_TOP = 1
 local TEXT_PADDING_BTM = 2
 
+local OFFSET_RIGHT = 3
+local OFFSET_DOWN = 18
+local OFFSET_LEFT = 2
+local OFFSET_UP = 2
+
 local defaultProps = {
 	Text = "Tooltip.defaultProps.Text",
 	MaxWidth = 200,
@@ -84,12 +89,31 @@ local function Tooltip(props, hooks)
 
 	local frameSize = Vector2.new(props.MaxWidth - TEXT_PADDING_SIDES * 2, math.huge)
 	local textSize = TextService:GetTextSize(props.Text, TEXT_SIZE, FONT, frameSize)
+	local fullSize = textSize + Vector2.new(TEXT_PADDING_SIDES * 2, TEXT_PADDING_BTM + TEXT_PADDING_TOP)
+	local buffer = 3 -- extra space required around/above/below
 
 	local target = nil
+	local anchor = Vector2.new(0, 0)
+	local offset = Vector2.new(OFFSET_RIGHT, OFFSET_DOWN)
 	if display and dummyRef.value then
 		local inst = dummyRef.value:getValue()
 		if inst ~= nil then
 			target = inst:FindFirstAncestorWhichIsA("LayerCollector")
+			local mouse = displayPos.value
+			if target ~= nil then
+				local spaceRight = target.AbsoluteSize.x - mouse.x - OFFSET_RIGHT
+				local spaceLeft = mouse.x - OFFSET_LEFT
+				if spaceRight < fullSize.x + buffer and spaceLeft > spaceRight then
+					anchor = Vector2.new(1, anchor.y)
+					offset = Vector2.new(-OFFSET_LEFT, offset.y)
+				end
+				local spaceBelow = target.AbsoluteSize.y - mouse.y - OFFSET_DOWN
+				local spaceAbove = mouse.y - OFFSET_UP
+				if spaceBelow < fullSize.y + buffer and spaceAbove > spaceBelow then
+					anchor = Vector2.new(anchor.x, 1)
+					offset = Vector2.new(offset.x, -OFFSET_UP)
+				end
+			end
 		end
 	end
 
@@ -135,11 +159,9 @@ local function Tooltip(props, hooks)
 			Tooltip = Roact.createElement("Frame", {
 				ZIndex = Constants.ZIndex.Tooltip,
 				BackgroundTransparency = 1,
-				Size = UDim2.fromOffset(
-					textSize.x + TEXT_PADDING_SIDES * 2,
-					textSize.y + TEXT_PADDING_TOP + TEXT_PADDING_BTM
-				),
-				Position = UDim2.fromOffset(displayPos.value.x + 3, displayPos.value.y + 18),
+				Size = UDim2.fromOffset(fullSize.x, fullSize.y),
+				AnchorPoint = anchor,
+				Position = UDim2.fromOffset(displayPos.value.x + offset.x, displayPos.value.y + offset.y),
 			}, {
 				Label = Roact.createElement("TextLabel", {
 					ZIndex = 1,
