d2ed70d8921babae839f5be3b5db20256c85dccb
sircfenner <sircfenner@users.noreply.github.com>
2023-04-30 18:45:54 +0100

    textinput: use hooks, prevent text overflow, truncate when not editing, add zindex + anchorpoint props

--- src/TextInput.lua
+++ src/TextInput.lua
@@ -1,107 +1,179 @@
+local TextService = game:GetService("TextService")
+
 local Packages = script.Parent.Parent
 local Roact = require(Packages.Roact)
+local Hooks = require(Packages.RoactHooks)
 
 local joinDictionaries = require(script.Parent.joinDictionaries)
 local withTheme = require(script.Parent.withTheme)
 
 local Constants = require(script.Parent.Constants)
-local TextInput = Roact.Component:extend("TextInput")
 
 local PLACEHOLDER_TEXT_COLOR = Color3.fromRGB(102, 102, 102) -- works for both themes
+local EDGE_PADDING_PX = 5
 
-local noop = function() end
-
-TextInput.defaultProps = {
+local defaultProps = { -- do we need this???????????????????????????????????????????????????????????????
 	Size = UDim2.new(1, 0, 0, 21),
-	LayoutOrder = 0,
 	Disabled = false,
 	Text = "",
 	PlaceholderText = "",
-	ClearTextOnFocus = true,
-	OnFocused = noop,
-	OnFocusLost = noop,
-	OnChanged = noop,
+	OnFocused = function() end,
+	OnFocusLost = function() end,
+	OnChanged = function() end,
 }
 
-function TextInput:init()
-	self:setState({
-		Hover = false,
-		Focused = false,
-	})
-	self.onInputBegan = function(_, inputObject)
-		if self.props.Disabled then
-			return
-		elseif inputObject.UserInputType == Enum.UserInputType.MouseMovement then
-			self:setState({ Hover = true })
-		end
+local function getTextWidth(text)
+	local frameSize = Vector2.new(math.huge, math.huge)
+	if #text == 0 then
+		return 0
 	end
-	self.onInputEnded = function(_, inputObject)
-		if self.props.Disabled then
-			return
-		elseif inputObject.UserInputType == Enum.UserInputType.MouseMovement then
-			self:setState({ Hover = false })
-		end
-	end
-	self.onFocused = function()
-		self:setState({ Focused = true })
-		self.props.OnFocused()
-	end
-	self.onFocusLost = function(rbx, enterPressed, inputObject)
-		self:setState({ Focused = false })
-		self.props.OnFocusLost(rbx.Text, enterPressed, inputObject)
-	end
-	self.onChanged = function(rbx)
-		self.props.OnChanged(rbx.Text)
-	end
+	return TextService:GetTextSize(text, Constants.TextSize, Constants.Font, frameSize).X + 1
 end
 
-function TextInput:render()
-	local padding = Roact.createElement("UIPadding", {
-		PaddingLeft = UDim.new(0, 5),
-		PaddingRight = UDim.new(0, 5),
-	})
+local function TextInput(props, hooks)
+	local hovered, setHovered = hooks.useState(false)
+	local focused, setFocused = hooks.useState(false)
+
+	local containerRef = hooks.useValue(Roact.createRef())
+	local innerOffset = hooks.useValue(0)
+	local cursorPosition, setCursorPosition = hooks.useState(-1)
+
+	local children = {
+		Padding = Roact.createElement("UIPadding", {
+			PaddingLeft = UDim.new(0, EDGE_PADDING_PX),
+			PaddingRight = UDim.new(0, EDGE_PADDING_PX),
+		}),
+	}
+
 	local mainModifier = Enum.StudioStyleGuideModifier.Default
 	local borderModifier = Enum.StudioStyleGuideModifier.Default
-	if self.props.Disabled then
+	if props.Disabled then
 		mainModifier = Enum.StudioStyleGuideModifier.Disabled
 		borderModifier = Enum.StudioStyleGuideModifier.Disabled
-	elseif self.state.Focused then
+	elseif focused then
 		borderModifier = Enum.StudioStyleGuideModifier.Selected
-	elseif self.state.Hover then
+	elseif hovered then
 		borderModifier = Enum.StudioStyleGuideModifier.Hover
 	end
+
+	local function onInputBegan(_, inputObject)
+		if props.Disabled then
+			return
+		elseif inputObject.UserInputType == Enum.UserInputType.MouseMovement then
+			setHovered(true)
+		end
+	end
+
+	local function onInputEnded(_, inputObject)
+		if props.Disabled then
+			return
+		elseif inputObject.UserInputType == Enum.UserInputType.MouseMovement then
+			setHovered(false)
+		end
+	end
+
+	local function onFocused()
+		setFocused(true)
+		props.OnFocused()
+	end
+
+	local function onFocusLost(rbx, enterPressed, inputObject)
+		setFocused(false)
+		props.OnFocusLost(rbx.Text, enterPressed, inputObject)
+	end
+
+	local function onTextChanged(rbx)
+		props.OnChanged(rbx.Text)
+	end
+
+	local function onCursorChanged(rbx)
+		setCursorPosition(rbx.CursorPosition)
+	end
+
+	local container = containerRef.value:getValue()
+	if not props.Disabled and container then
+		local min = EDGE_PADDING_PX
+		local max = container.AbsoluteSize.X - EDGE_PADDING_PX
+
+		local text = string.sub(props.Text, 1, cursorPosition - 1)
+		local offset = getTextWidth(text) + EDGE_PADDING_PX
+
+		local innerArea = max - min
+		local fullOffset = offset + innerOffset.value
+		local fullTextWidth = getTextWidth(props.Text)
+		if fullTextWidth <= innerArea or not focused then
+			innerOffset.value = 0
+		else
+			if fullOffset < min then
+				local adjust = min - fullOffset
+				innerOffset.value += adjust
+			elseif fullOffset > max then
+				local adjust = fullOffset - max
+				innerOffset.value -= adjust
+			end
+			innerOffset.value = math.max(innerOffset.value, innerArea - fullTextWidth)
+		end
+	else
+		innerOffset.value = 0
+	end
+
+	local textFieldSize = UDim2.fromScale(1, 1)
+	if container and focused then
+		local fullTextWidth = getTextWidth(props.Text)
+		textFieldSize = UDim2.new(
+			UDim.new(0, math.max(container.AbsoluteSize.X, fullTextWidth + EDGE_PADDING_PX * 2)),
+			UDim.new(1, 0)
+		)
+	end
+
 	return withTheme(function(theme)
 		local textFieldProps = {
-			Size = self.props.Size,
-			Position = self.props.Position,
-			BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.InputFieldBackground, mainModifier),
-			BorderColor3 = theme:GetColor(Enum.StudioStyleGuideColor.InputFieldBorder, borderModifier),
-			BorderMode = Enum.BorderMode.Inset,
-			LayoutOrder = self.props.LayoutOrder,
+			Size = textFieldSize,
+			Position = UDim2.fromOffset(innerOffset.value, 0),
+			BackgroundTransparency = 1,
 			Font = Constants.Font,
-			Text = self.props.Text,
+			Text = props.Text,
 			TextSize = Constants.TextSize,
 			TextColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainText, mainModifier),
 			TextXAlignment = Enum.TextXAlignment.Left,
+			TextTruncate = if focused then Enum.TextTruncate.None else Enum.TextTruncate.AtEnd,
 		}
-		return self.props.Disabled and Roact.createElement("TextLabel", textFieldProps, { Padding = padding })
+
+		local textField = props.Disabled and Roact.createElement("TextLabel", textFieldProps, children)
 			or Roact.createElement(
 				"TextBox",
 				joinDictionaries(textFieldProps, {
-					PlaceholderText = self.props.PlaceholderText,
+					PlaceholderText = props.PlaceholderText,
 					PlaceholderColor3 = PLACEHOLDER_TEXT_COLOR,
-					ClearTextOnFocus = self.props.ClearTextOnFocus,
-					[Roact.Event.Focused] = self.onFocused,
-					[Roact.Event.FocusLost] = self.onFocusLost,
-					[Roact.Event.InputBegan] = self.onInputBegan,
-					[Roact.Event.InputEnded] = self.onInputEnded,
-					[Roact.Change.Text] = self.onChanged,
+					ClearTextOnFocus = props.ClearTextOnFocus,
+					MultiLine = false,
+					[Roact.Event.Focused] = onFocused,
+					[Roact.Event.FocusLost] = onFocusLost,
+					[Roact.Event.InputBegan] = onInputBegan,
+					[Roact.Event.InputEnded] = onInputEnded,
+					[Roact.Change.Text] = onTextChanged,
+					[Roact.Change.CursorPosition] = onCursorChanged,
 				}),
-				{
-					Padding = padding,
-				}
+				children
 			)
+
+		return Roact.createElement("Frame", {
+			AnchorPoint = props.AnchorPoint,
+			Position = props.Position,
+			Size = props.Size,
+			LayoutOrder = props.LayoutOrder,
+			ZIndex = props.ZIndex,
+			BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.InputFieldBackground, mainModifier),
+			BorderColor3 = theme:GetColor(Enum.StudioStyleGuideColor.InputFieldBorder, borderModifier),
+			BorderMode = Enum.BorderMode.Inset,
+			ClipsDescendants = true,
+			[Roact.Ref] = containerRef.value,
+		}, {
+			TextField = textField,
+		})
 	end)
 end
 
-return TextInput
+return Hooks.new(Roact)(TextInput, {
+	defaultProps = defaultProps,
+})
--- src/TextInput.story.lua
+++ src/TextInput.story.lua
@@ -1,13 +1,26 @@
 local Packages = script.Parent.Parent
+
 local Roact = require(Packages.Roact)
+local Hooks = require(Packages.RoactHooks)
 
 local TextInput = require(script.Parent.TextInput)
+
+local Helper = Hooks.new(Roact)(function(props, hooks)
+	local text, setText = hooks.useState(props.InitText)
+	return Roact.createElement(TextInput, {
+		LayoutOrder = props.LayoutOrder,
+		PlaceholderText = props.PlaceholderText,
+		Disabled = props.Disabled,
+		Text = text,
+		OnChanged = setText,
+	})
+end)
 
 return function(target)
 	local element = Roact.createElement("Frame", {
 		AnchorPoint = Vector2.new(0.5, 0.5),
 		Position = UDim2.fromScale(0.5, 0.5),
-		Size = UDim2.fromScale(0.7, 0.7),
+		Size = UDim2.new(0, 150, 1, 0),
 		BackgroundTransparency = 1,
 	}, {
 		Layout = Roact.createElement("UIListLayout", {
@@ -17,15 +30,15 @@ return function(target)
 			HorizontalAlignment = Enum.HorizontalAlignment.Center,
 			VerticalAlignment = Enum.VerticalAlignment.Center,
 		}),
-		Input0 = Roact.createElement(TextInput, {
+		Input0 = Roact.createElement(Helper, {
 			LayoutOrder = 0,
 			PlaceholderText = "Enabled",
 		}),
-		TextInput1 = Roact.createElement(TextInput, {
+		Input1 = Roact.createElement(Helper, {
 			LayoutOrder = 1,
 			Disabled = true,
 			PlaceholderText = "Disabled",
-			Text = "Disabled",
+			InitText = "Disabled",
 		}),
 	})
 	local handle = Roact.mount(element, target)
