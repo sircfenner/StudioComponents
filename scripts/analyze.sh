ce8d3d18419b100788526ad658fbd57edf964892
sircfenner <sircfenner@users.noreply.github.com>
2025-01-03 20:47:07 +0000

    update luau-analyze setup

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -8,6 +8,4 @@ if [ ! -f "$TYPES_FILE" ]; then
     curl https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua > $TYPES_FILE
 fi
 
-luau-lsp analyze --base-luaurc=.luaurc --settings=.luau-analyze.json \
-    --definitions=$TYPES_FILE \
-    src
+luau-lsp analyze --base-luaurc=.luaurc --definitions=$TYPES_FILE src
